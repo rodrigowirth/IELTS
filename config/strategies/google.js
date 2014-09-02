382becd3350597bb7a06845766efb585609ab6fe
rodrigowirth <rodrigowirth90@gmail.com>
2014-09-02 10:44:46 -0300

    Grant access to jon.holloway@bravisoftware.com

--- config/strategies/google.js
+++ config/strategies/google.js
@@ -19,7 +19,7 @@ module.exports = function() {
 		},
 		function (req, accessToken, refreshToken, profile, done) {
 
-		    if (profile.emails[0].value !== 'rodrigo.wirth@bravisoftware.com') {
+		    if (profile.emails[0].value !== 'rodrigo.wirth@bravisoftware.com' && profile.emails[0].value !== 'jon.holloway@bravisoftware.com') {
 		        return done({
 		            status: 401,
 		            message: 'You have to use a Bravi Software account.'
