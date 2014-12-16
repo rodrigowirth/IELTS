773b81d84dd374067e1132700697c9f25cbfd708
rodrigowirth <rodrigowirth90@gmail.com>
2014-12-16 15:54:43 -0200

    Adding manage user menu

--- public/modules/usersadmin/config/usersadmin.client.config.js
+++ public/modules/usersadmin/config/usersadmin.client.config.js
@@ -4,5 +4,6 @@ angular.module('usersadmin').run(['Menus',
 	function (Menus) {
 	    // Set top bar menu items
 	    Menus.addMenuItem('topbar', 'Users', 'usersadmin', 'dropdown', '/usersadmin', false, ['admin']);
+	    Menus.addSubMenuItem('topbar', 'usersadmin', 'Manage', 'usersadmin', '', false, ['admin']);
 	}
 ]);
