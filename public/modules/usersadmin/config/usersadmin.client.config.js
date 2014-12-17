5e430eb0d6bfbeeee299326e93af505ece07a225
rodrigowirth <rodrigowirth90@gmail.com>
2014-12-17 14:40:58 -0200

    Adding scores for learners report

--- public/modules/usersadmin/config/usersadmin.client.config.js
+++ public/modules/usersadmin/config/usersadmin.client.config.js
@@ -3,7 +3,7 @@
 angular.module('usersadmin').run(['Menus',
 	function (Menus) {
 	    // Set top bar menu items
-	    Menus.addMenuItem('topbar', 'Users', 'usersadmin', 'dropdown', '/usersadmin', false);
-	    Menus.addSubMenuItem('topbar', 'usersadmin', 'Manage', 'usersadmin', '', false);
+	    Menus.addMenuItem('topbar', 'Users', 'usersadmin', 'dropdown', '/usersadmin', false, ['admin']);
+	    Menus.addSubMenuItem('topbar', 'usersadmin', 'Manage', 'usersadmin', '', false, ['admin']);
 	}
 ]);
